321871881c64dc31cbf7cffd1f93117515b32d97
221B502 <152085997+PRIYAKUSHW@users.noreply.github.com>
2025-01-24 21:23:49 +0530

    Add files via upload

---
+++ LE0.java
@@ -0,0 +1,23 @@
+
+public class Main
+{
+    int age;
+    satring name;
+    void setAge (int age){
+
+        this.age=age;
+    }
+    void set age(String name){
+        thid.name=name;
+    } int getAge(){
+        return this.age;
+    }
+    
+	public static void main(String[] args) {
+	    Main m1 =new Main();
+	    m1.setName("priya");
+	    m1.setAge("21");
+	    
+		System.out.println(m1.getName()+""+m1.getAge());
+	}
+}
